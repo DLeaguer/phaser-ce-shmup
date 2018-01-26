5b5d9ce39028a84816f8b2703dad968f7a2fcacb
DLeaguer <immostlikely@hotmail.com>
2018-01-25 19:04:11 -1000

    addSpaceship

--- js/game.js
+++ js/game.js
@@ -1,3 +1,31 @@
+//jshint esversion: 6
+
 (Phaser => {
-  console.log(Phaser);
+  // console.log(Phaser);
+  const GAME_WIDTH = 460;
+  const GAME_HEIGHT = 600;
+  const GAME_CONTAINER_ID = 'game';
+  const GFX = 'gfx';
+
+
+  const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
+
+  let player;
+
+
+
+  // Core game methods
+  function preload() {
+  	game.load.spritesheet(GFX, '../assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
+
+  };
+
+  function create() {
+  	player = game.add.sprite(100,100, GFX, 8);
+  }
+
+  function update() {
+    
+  }
+
 })(window.Phaser);
