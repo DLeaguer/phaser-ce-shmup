2ffac3ba165a2097f1eb0a00e362fa3b9381a0a7
DLeaguer <immostlikely@hotmail.com>
2018-01-25 19:16:20 -1000

    addMovement

--- js/game.js
+++ js/game.js
@@ -6,11 +6,16 @@
   const GAME_HEIGHT = 600;
   const GAME_CONTAINER_ID = 'game';
   const GFX = 'gfx';
+  const INITIAL_MOVESPEED = 4;
+
 
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
 
+
+
   let player;
+  let cursors;
 
 
 
@@ -21,11 +26,33 @@
   };
 
   function create() {
+  	cursors = game.input.keyboard.createCursorKeys();
   	player = game.add.sprite(100,100, GFX, 8);
-  }
+  	player.moveSpeed = INITIAL_MOVESPEED;
+  };
+
 
   function update() {
-    
+    handlePlayerMovement();
   }
+
+  function handlePlayerMovement() {
+    switch( true ){
+      case cursors.left.isDown:
+        player.x -= player.moveSpeed;
+        break;
+      case cursors.right.isDown:
+        player.x += player.moveSpeed;
+        break;
+    }
+    switch( true ){
+      case cursors.down.isDown:
+        player.y += player.moveSpeed;
+        break;
+      case cursors.up.isDown:
+        player.y -= player.moveSpeed;
+        break;
+    }
+  };
 
 })(window.Phaser);
